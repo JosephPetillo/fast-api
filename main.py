90d01587a14435675a412b5bec80e320d1fcc903
JosephPetillo <joseph.petillo.1@stonybrook.edu>
2023-05-12 04:23:16 +0000

    tested data filtering

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
-    return 'this is a API service for MN ICD code details'
+    return 'this is a API service for MN provider code details'
 
 @app.route('/preview', methods=["GET"])
 def preview():
@@ -15,19 +15,19 @@ def preview():
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=['GET'])
+@app.route('/provider/<value>', methods=['GET'])
 def icdcode(value):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['UNIQUE_PROVIDERS'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
     else: 
         return filtered.to_json(orient="records")
 
-@app.route('/icd/<value>/sex/<value2>')
+@app.route('/provider/<value>/cd/<value2>')
 def icdcode2(value, value2):
-    filtered = df[df['principal_diagnosis_code'] == value]
-    filtered2 = filtered[filtered['sex'] == value2]
+    filtered = df[df['UNIQUE_PROVIDERS'] == value]
+    filtered2 = filtered[filtered['PROVIDER_TYPE_CD'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
     else: 
