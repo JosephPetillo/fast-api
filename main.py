7ad422a8794a53e6319a6a172e2d70793f7222bb
JosephPetillo <joseph.petillo.1@stonybrook.edu>
2023-05-12 03:46:14 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from time import time
-from fastapi import FastAPI, __version__
+from flask import Flask, request 
+import pandas as pd 
 
-app = FastAPI() # This is what will be refrenced in config
+df = pd.read_csv('./data/Services2019Mn.csv')
 
-@app.get('/')
-async def hello():
-    return {'res': 'pong', 'version': __version__, "time": time()}
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def home():
+    return 'this is a API service for MN Services'
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.route('/icd/<value>', methods=['GET'])
+def icdcode(value):
+    print('value: ', value)
+    filtered = df[df['age_group_code'] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered.to_json(orient="records")
+
+@app.route('/icd/<value>/sex/<value2>')
+def icdcode2(value, value2):
+    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered2 = filtered[filtered['sex'] == value2]
+    if len(filtered2) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered2.to_json(orient="records")    
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
